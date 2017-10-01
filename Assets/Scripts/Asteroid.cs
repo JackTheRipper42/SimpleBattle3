0a1c9c03ec3271c3fc3f3d2c3ea29c8c99c745de
Jack the Ripper <andreastheripper@aol.de>
2017-10-01 16:54:10 +0200

    asteroids have random rotations

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Asteroid : Entity
+{
+    public float Speed = 1;
+
+    protected override void Start()
+    {
+        base.Start();
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * Speed;
+    }
+}
