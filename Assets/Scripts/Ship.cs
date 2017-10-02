bc35cc7520e957c4e29ef57c872f10e5d8b51206
Jack the Ripper <andreastheripper@aol.de>
2017-10-02 14:40:01 +0200

    ship state visualization improved

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,48 +5,61 @@ public class Ship : Entity
     public int MovementRange = 4;
     public int FireRange = 1;
     public Side Side;
-    public GameObject TargetMarker;
     public AudioSource FireAudio;
     public AudioSource ThrusterAudio;
+    public GameObject UIPrefab;
+
+    private ShipUI _shipUI;
 
     public bool CanMove { get; private set; }
 
     public bool CanFire { get; private set; }
 
+    protected virtual void Awake()
+    {
+        var uiObject = Instantiate(UIPrefab, transform);
+        _shipUI = uiObject.GetComponent<ShipUI>();
+    }
+
     protected override void Start()
     {
         base.Start();
-        DisableTargetMarker();
-        CanMove = true;
-        CanFire = true;
+        StartTurn();
     }
 
     public new void Move(GridPosition position)
     {
         base.Move(position);
         CanMove = false;
+        _shipUI.DisableCanMoveMarker();
     }
 
     public void Attack(Ship target)
     {
         target.Kill();
         CanFire = false;
+        _shipUI.DisableCanFireMarker();
     }
 
     public void EnableTargetMarker()
     {
-        TargetMarker.SetActive(true);
+        _shipUI.EnableTargetMarker();
     }
 
     public void DisableTargetMarker()
     {
-        TargetMarker.SetActive(false);
+        _shipUI.DisableTargetMarker();
     }
 
     public void StartTurn()
     {
         CanFire = true;
         CanMove = true;
+        if (GameManager.PlayerSide == Side)
+        {
+            _shipUI.EnableCanFireMarker();
+            _shipUI.EnableCanMoveMarker();
+        }
     }
 
     public override bool IsObstacle(Side side)
---
+++ Assets/Scripts/ShipUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class ShipUI : MonoBehaviour
+{
+    public GameObject TargetMarker;
+    public GameObject CanMoveMarker;
+    public GameObject CanFireMarker;
+
+    protected virtual void Awake()
+    {
+        DisableTargetMarker();
+        DisableCanFireMarker();
+        DisableCanMoveMarker();
+    }
+
+    public void EnableTargetMarker()
+    {
+        TargetMarker.SetActive(true);
+    }
+
+    public void DisableTargetMarker()
+    {
+        TargetMarker.SetActive(false);
+    }
+
+    public void EnableCanMoveMarker()
+    {
+        CanMoveMarker.SetActive(true);
+    }
+
+    public void DisableCanMoveMarker()
+    {
+        CanMoveMarker.SetActive(false);
+    }
+
+    public void EnableCanFireMarker()
+    {
+        CanFireMarker.SetActive(true);
+    }
+
+    public void DisableCanFireMarker()
+    {
+        CanFireMarker.SetActive(false);
+    }
+}
