5c5ab255e42b1e956781a5ab21587b3abd3ccc20
Jack the Ripper <andreastheripper@aol.de>
2017-10-20 22:32:10 +0200

    npc ships do not move to occupied positions to attack the player

--- Assets/Scripts/IdiotAI.cs
+++ Assets/Scripts/IdiotAI.cs
@@ -11,38 +11,63 @@ public class IdiotAI
 
         foreach (var ship in ownShips)
         {
-            var enemy = GetCloestEnemy(ship, enemyShips);
-
-            if (GridPosition.Distance(ship.Position, enemy.Position) <= ship.FireRange)
+            var blackList = new List<Ship>();
+            bool selectNewEnemy;
+            do
             {
-                yield return ship.Attack(enemy);
-            }
-            else
-            {
-                var obstacles = entities
-                    .Except(ownShips)
-                    .Where(entity => entity != enemy)
-                    .Select(entity => entity.Position)
-                    .ToList();
-
-                var fullPath = astar.Calculate(ship.Position, enemy.Position, obstacles);
-                var pathToFirePosition = fullPath.Take(fullPath.Count - ship.FireRange).ToList();
-
-                var path = pathToFirePosition.Count > ship.MovementRange + 2
-                    ? pathToFirePosition.Take(ship.MovementRange + 1).ToList()
-                    : pathToFirePosition;
-                yield return ship.Move(path, speed);
+                selectNewEnemy = false;
+                var enemy = GetNearestEnemy(ship, enemyShips.Except(blackList));
 
                 if (GridPosition.Distance(ship.Position, enemy.Position) <= ship.FireRange)
                 {
                     yield return ship.Attack(enemy);
                 }
-            }
-        }
+                else
+                {
+                    var obstacles = entities
+                        .Except(ownShips)
+                        .Where(entity => entity != enemy)
+                        .Select(entity => entity.Position)
+                        .ToList();
 
+                    var destinations = GetDestinations(enemy.Position, ship.FireRange)
+                        .Except(entities.Select(entity => entity.Position))
+                        .Distinct();
+
+                    IList<GridPosition> shortestPath = null;
+                    foreach (var destination in destinations)
+                    {
+                        var path = astar.Calculate(ship.Position, destination, obstacles);
+                        if (shortestPath == null || path.Count < shortestPath.Count)
+                        {
+                            shortestPath = path;
+                        }
+                    }
+
+                    if (shortestPath != null)
+                    {
+                        var path = shortestPath.Count > ship.MovementRange + 2
+                            ? shortestPath.Take(ship.MovementRange + 1).ToList()
+                            : shortestPath;
+                        yield return ship.Move(path, speed);
+
+                        if (GridPosition.Distance(ship.Position, enemy.Position) <= ship.FireRange)
+                        {
+                            yield return ship.Attack(enemy);
+                        }
+
+                    }
+                    else
+                    {
+                        blackList.Add(enemy);
+                        selectNewEnemy = true;
+                    }
+                }
+            } while (selectNewEnemy && blackList.Count < enemyShips.Count);
+        }
     }
 
-    private static Ship GetCloestEnemy(Entity self, IEnumerable<Ship> enemies)
+    private static Ship GetNearestEnemy(Entity self, IEnumerable<Ship> enemies)
     {
         int minDistance = int.MaxValue;
         Ship closest = null;
@@ -59,4 +84,24 @@ public class IdiotAI
 
         return closest;
     }
+
+    private static List<GridPosition> GetDestinations(GridPosition position, int range)
+    {
+        var destinations = new List<GridPosition>();
+
+        if (range <= 0)
+        {
+            return destinations;
+        }
+
+        foreach (var neighbor in position.Neighbors)
+        {
+            destinations.Add(neighbor);
+            destinations.AddRange(GetDestinations(neighbor, range - 1));
+
+        }
+
+        return destinations;
+    }
+
 }
