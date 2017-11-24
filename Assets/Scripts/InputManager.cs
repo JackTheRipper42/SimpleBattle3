db3a0bad9eb7ee957c691d6ecfc4c3c7b220e814
Jack the Ripper <andreastheripper@aol.de>
2017-11-24 19:47:55 +0100

    units and grid positions behind the ui cannot be clicked

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
@@ -6,13 +9,44 @@ public class InputManager : MonoBehaviour
     public LayerMask GridLayerMask;
     public float ScrollSpeed = 1;
     public Button LoadButton;
+    public GameObject UI;
 
     private MissionManager _missionManager;
+    private Rect[] _uiRectangles;
 
     protected virtual void Start()
     {
         _missionManager = FindObjectOfType<MissionManager>();
         LoadButton.interactable = GameManager.Instance.SaveGameAvailable;
+
+        var allRectangles = UI
+            .GetComponentsInChildren<UIBehaviour>()
+            .Select(ui => (RectTransform) ui.transform)
+            .Select(rect =>
+            {
+                var corners = new Vector3[4];
+                rect.GetWorldCorners(corners);
+                return new Rect(corners[0], corners[2] - corners[0]);
+            })
+            .ToArray();
+
+        var redundant = new List<Rect>();
+        for (var i = 0; i < allRectangles.Length; i++)
+        {
+            for (var j = 0; j < allRectangles.Length; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+                if (allRectangles[i].Contains(allRectangles[j].min) && allRectangles[i].Contains(allRectangles[j].max))
+                {
+                    redundant.Add(allRectangles[j]);
+                }
+            }
+        }
+
+        _uiRectangles = allRectangles.Except(redundant).ToArray();
     }
 
     protected virtual void Update()
@@ -24,7 +58,7 @@ public class InputManager : MonoBehaviour
 
         _missionManager.MoveCamera(inputScroll * ScrollSpeed * Time.deltaTime);
 
-        if (_missionManager.BlockUI)
+        if (_missionManager.BlockUI || IsMouseOverUI())
         {
             return;
         }
@@ -92,5 +126,10 @@ public class InputManager : MonoBehaviour
         }
 
         GameManager.Instance.MainMenu();
+    }
+
+    private bool IsMouseOverUI()
+    {
+        return _uiRectangles.Any(rect => rect.Contains(Input.mousePosition));
     }
 }
